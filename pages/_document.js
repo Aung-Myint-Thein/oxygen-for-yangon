4ceeccfca6fa3769652f574b5052a344c9f1745e
aung <thein.aungmyint@gmail.com>
2021-08-22 17:20:02 +0630

    changed new favicon and added descriptions

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="thumbnail" content="https://res.cloudinary.com/myanlearn/image/upload/v1629629316/MMD-Link-Preview.jpg" />
+          <meta property="og:image" content="https://res.cloudinary.com/myanlearn/image/upload/v1629629316/MMD-Link-Preview.jpg" />
+          <meta name="description" content="COVID အတွက် အောက်စီဂျင် ဝယ်၊ ဖြည့်၊ ငှါးနိုင်သော နေရာများ၊ ဆေးဆိုင်များ၊ ဓာတ်မှန်နှင့် ဓာတ်ခွဲခန်းများကို လွယ်ကူတိကျအချိန်မီ ရှာဖွေလိုက်ပါ။" />
+          <meta name="keywords" content="myanmar,oxygen,medical,directory,covid" />
+          <meta property="og:title" content="Myanmar Medical Directory | Essentials for COVID-19 treatment and recovery" />
+          <meta property="og:description" content="COVID အတွက် အောက်စီဂျင် ဝယ်၊ ဖြည့်၊ ငှါးနိုင်သော နေရာများ၊ ဆေးဆိုင်များ၊ ဓာတ်မှန်နှင့် ဓာတ်ခွဲခန်းများကို လွယ်ကူတိကျအချိန်မီ ရှာဖွေလိုက်ပါ။" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
